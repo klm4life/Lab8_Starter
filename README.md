5baa271ce6ea215e34b1a432c3562cdd486e364f
klm4life <mikophg2001@gmail.com>
2022-11-21 18:24:16 -0800

    finished explore pog

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Lab 8 - Starter
 Michael Phung
-1.) You would fit automated tests within a Github action that runs whenever code is pushed because it allows for you to automatically test new versions of your code as updates get pushed to your repository.
+
+1) You would fit automated tests within a Github action that runs whenever code is pushed because it allows for you to automatically test new versions of your code as updates get pushed to your repository.
 
 2) Yes
+
+3) No because testing a feature is more suited for end to end testing since it involves a user writing and sending a message to another user which would be best tested by emulating user actions from start to finish.
+
+4) Yes because you are testing an individual part of the code in an encapsulated unit which will not interact with other parts of the codebase which is what unit tests are for.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,83 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('valid phone number', () => {
+    expect(functions.isPhoneNumber('626-737-9109')).toBe(true);
+});
+
+test('valid phone number', () => {
+    expect(functions.isPhoneNumber('(919)-129-1234')).toBe(true);
+});
+
+test('invalid phone number', () => {
+    expect(functions.isPhoneNumber('jungle diff')).toBe(false);
+});
+
+test('invalid phone number', () => {
+    expect(functions.isPhoneNumber('6')).toBe(false);
+});
+
+test('valid email', () => {
+    expect(functions.isEmail('mphung@ucsd.edu')).toBe(true);
+});
+
+test('valid email', () => {
+    expect(functions.isEmail('xyz1995@gmail.edu')).toBe(true);
+});
+
+test('invalid email', () => {
+    expect(functions.isEmail('gmail.com@hello')).toBe(false);
+});
+
+test('invalid email', () => {
+    expect(functions.isEmail('')).toBe(false);
+});
+
+test('strong password', () => {
+    expect(functions.isStrongPassword('fsdsdsdmz_')).toBe(true);
+});
+
+test('strong password', () => {
+    expect(functions.isStrongPassword('msida12')).toBe(true);
+});
+
+test('not strong password', () => {
+    expect(functions.isStrongPassword('fs_')).toBe(false);
+});
+
+test('not strong password', () => {
+    expect(functions.isStrongPassword('@@lolskddsds')).toBe(false);
+});
+
+test('is date', () => {
+    expect(functions.isDate('12/23/1992')).toBe(true);
+});
+
+test('is date', () => {
+    expect(functions.isDate('1/1/1111')).toBe(true);
+});
+
+test('is not date', () => {
+    expect(functions.isDate('xx/xx/2001')).toBe(false);
+});
+
+test('is not date', () => {
+    expect(functions.isDate('fsds_')).toBe(false);
+});
+
+test('is hex color', () => {
+    expect(functions.isHexColor('#0000FF')).toBe(true);
+});
+
+test('is hex color', () => {
+    expect(functions.isHexColor('#fc9')).toBe(true);
+});
+
+test('is not hex color', () => {
+    expect(functions.isHexColor('#blah')).toBe(false);
+});
+
+test('is not hex color', () => {
+    expect(functions.isHexColor('#truemf')).toBe(false);
+});
